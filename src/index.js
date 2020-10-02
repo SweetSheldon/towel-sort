1ee2d29b02f07bb7fda66b0f788621b5dce9ff8d
SweetSheldon <sheldongamesandlp@gmail.com>
2020-10-03 01:00:45 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!(matrix instanceof Array)||matrix=='undefined'||!(Array.isArray(matrix) && matrix.length)){return []}  
+  var number = '';
+  for(i=0;i<matrix.length;i++){
+    if(i%2==0){ number+=(pushElem(matrix[i])) }
+    else{ number+=(pushElem(matrix[i].reverse())) }
+  }
+  var result=number.slice(0,-1);
+  return (result).split(" ");
+}
+
+function pushElem(arr){
+  var res ='';
+  arr.forEach(element => {
+    res+=element.toString()+' ';
+  });
+  return res;
 }
